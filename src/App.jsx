f43c0228bcd135c872f6d0bd31cb4a697927b9e1
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-28 23:06:07 +0000

    Import pages in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import LocationPage from './components/pages/LocationPage/LocationPage'
+import SalaryPage from './components/pages/SalaryPage/SalaryPage'
+import NumberPage from './components/pages/NumberPage/NumberPage'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        hi there
-      </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LocationPage />} />
+          <Route path="/salary" element={<SalaryPage />} />
+          <Route path="/number" element={<NumberPage />} />
+        </Routes>
+      </Router>
     </>
   )
 }
