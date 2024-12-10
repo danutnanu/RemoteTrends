5e312714db1d14f7f7c3a40105d7454e7d238b50
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-12-10 11:48:22 +0000

    Update Sidebar page

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -15,21 +15,21 @@ const Sidebar = () => {
         <BootstrapNavbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShow} />
       </BootstrapNavbar>
 
-      <Offcanvas show={show} onHide={handleClose} placement="start" id="offcanvasNavbar">
+      <Offcanvas show={show} onHide={handleClose} placement="start" id="offcanvasNavbar" className="offcanvas-custom">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Menu</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <Nav className="flex-column">
-            <Nav.Link as={Link} to="/">Location Page</Nav.Link>
-            <Nav.Link as={Link} to="/salary">Salary Page</Nav.Link>
-            <Nav.Link as={Link} to="/number">Number Page</Nav.Link>
+            <Nav.Link as={Link} to="/" onClick={handleClose}>Location Page</Nav.Link>
+            <Nav.Link as={Link} to="/salary" onClick={handleClose}>Salary Page</Nav.Link>
+            <Nav.Link as={Link} to="/number" onClick={handleClose}>Number Page</Nav.Link>
           </Nav>
         </Offcanvas.Body>
       </Offcanvas>
 
-      <div className="sidebar d-none d-lg-block" style={{ width: '250px', height: '100vh', position: 'fixed', backgroundColor: '#24303f', padding: '20px', top: 0 }}>
-        <div className="sidebar-header mb-5 p-1 border-bottom" style={{ display: 'flex', alignItems: 'center' }}>
+      <div className="sidebar d-none d-lg-block">
+        <div className="sidebar-header mb-5 p-1 border-bottom">
           <img src="/public/favic.ico" alt="RemoteTrends Logo" style={{ width: '30px', height: '30px', marginRight: '10px' }} />
           <span style={{ color: '#fff', fontSize: '1.5em', fontWeight: 'bold' }}>RemoteTrends</span>
         </div>
