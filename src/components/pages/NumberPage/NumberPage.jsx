0a81962a607ab067eb826818590ff2b1eef31d2c
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-12-16 22:13:30 +0000

    Add header to NumberPage component

--- src/components/pages/NumberPage/NumberPage.jsx
+++ src/components/pages/NumberPage/NumberPage.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const NumberPage = () => {
     return (
-        <div>
-            <h1>Number Page</h1>
+        <div className="main-content">
+            <div className="header pe-5 p-3 fixed-top">
+                <img src="/public/favic.ico" alt="RemoteTrends Logo" style={{ width: '30px', height: '30px', marginRight: '10px', marginLeft: '30px' }} />
+                <span style={{ color: '#fff', fontSize: '1.6em', fontWeight: 'bold' }}>RemoteTrends</span>
+            </div>
+            <div className="charts-container px-3">
+                <div id="chart-container" className="pt-3 rounded" style={{ width: '100%', height: '400px' }}></div>
+            </div>
         </div>
     );
 };
