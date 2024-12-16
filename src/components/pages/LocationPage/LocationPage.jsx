958dd367b69817cfae5ad5b12b446faf8430f219
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-12-16 22:13:12 +0000

    Add header to LocationPage component

--- src/components/pages/LocationPage/LocationPage.jsx
+++ src/components/pages/LocationPage/LocationPage.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const LocationPage = () => {
     return (
-        <div>
-            <h1>Job Counts by Location</h1>
+        <div className="main-content">
+        <div className="header pe-5 p-3 fixed-top">
+            <img src="/public/favic.ico" alt="RemoteTrends Logo" style={{ width: '30px', height: '30px', marginRight: '10px', marginLeft: '30px' }} />
+            <span style={{ color: '#fff', fontSize: '1.6em', fontWeight: 'bold' }}>RemoteTrends</span>
         </div>
+        <div className="charts-container px-3">
+            <div id="chart-container" className="pt-3 rounded" style={{ width: '100%', height: '400px' }}></div>
+        </div>
+    </div>
     );
 };
 
