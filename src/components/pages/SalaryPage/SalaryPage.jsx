ba8b1d24d3f4c424c6702d19c0123488399bac49
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-12-18 19:48:36 +0000

    Refactor SalaryPage: Remove header, adjust layout for responsive design
    - Removed header styles and fixed top positioning in SalaryPage.jsx
    - Updated main-content in SalaryPage.css to use full width and removed margin-left
    - Added media query for main-content to adjust layout on larger screens

--- src/components/pages/SalaryPage/SalaryPage.jsx
+++ src/components/pages/SalaryPage/SalaryPage.jsx
@@ -115,9 +115,9 @@ const SalaryPage = () => {
 
     return (
         <div className="main-content">
-            <div className="header pe-5 p-3 fixed-top">
-                <img src="/public/favic.ico" alt="RemoteTrends Logo" style={{ width: '30px', height: '30px', marginRight: '10px', marginLeft: '30px' }} />
-                <span style={{ color: '#fff', fontSize: '1.6em', fontWeight: 'bold' }}>RemoteTrends</span>
+            <div className="pe-5 pt-5 text-center">
+                <img src="/public/favic.ico" alt="RemoteTrends Logo" style={{ width: '40px', height: '30px', marginRight: '20px', marginBottom: '10px' }} />
+                <span style={{ color: '#fff', fontSize: '1.8em', fontWeight: 'bold' }}>RemoteTrends</span>
             </div>
             <div className="charts-container px-3">
                 <div id="chart-container" className="pt-3 rounded" style={{ width: '100%', height: '400px' }}></div>
