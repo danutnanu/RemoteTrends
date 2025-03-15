f8839c00bf5b38b4506261f5ad1130b34d76da4c
danut nanu <125583127+danutnanu@users.noreply.github.com>
2025-03-15 07:09:50 +0000

    Update chart arrangement for responsive design across different devices

--- src/components/pages/SalaryPage/SalaryPage.jsx
+++ src/components/pages/SalaryPage/SalaryPage.jsx
@@ -123,19 +123,21 @@ const SalaryPage = () => {
     return (
         <div className="main-content">
             <div className="fixed-top fixed-header text-center">
-                <div className="title-container rounded-3 p-2 mt-3">
+                <div className="title-container rounded-3 p-1 mt-3">
                     <img src="/favic.ico" alt="RemoteTrends Logo" style={{ width: '40px', height: '40px', marginRight: '5px', marginBottom: '5px' }} />
                     <span className="title-remote" style={{ fontSize: '1.8em', fontWeight: 'bold' }}>Remote</span>
-                    <div className="title-trends rounded-4 py-1 px-3 mx-1">
+                    <div className="title-trends rounded-4 py-2 px-3 mx-1">
                         <span style={{ fontSize: '1.8em', fontWeight: 'bold' }}>Trends</span>
                     </div>
                 </div>
             </div>
-            <div className="charts-container px-3">
-                <TopJobsChart />
+            <div className="charts-container border border-info">
+                <div className="top-charts">
+                    <TopJobsChart />
+                    <ScatterPlot />
+                </div>
                 <div id="chart-container" className="pt-3 mb-5 rounded border border-3" style={{ width: '100%', height: '400px' }}></div>
             </div>
-            <ScatterPlot />
         </div>
     );
 };
