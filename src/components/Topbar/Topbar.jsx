0d665315e83090a80719c0093f74dd32ac8211df
danut nanu <125583127+danutnanu@users.noreply.github.com>
2025-03-19 17:00:45 +0000

    Update Topbar.jsx: Import CSS, adjust styling with reduced font size, margins and padding for better layout.

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-// import * as echarts from 'echarts';
+import './Topbar.css'
 
 const Topbar = () => {
   return (
     <>
-      <div className="fixed-top fixed-header text-center border border-info">
-        <div className="title-container rounded-3 p-1 mt-1">
-          <img src="/favic.ico" alt="RemoteTrends Logo" style={{ width: '40px', height: '40px', marginRight: '5px', marginBottom: '5px' }} />
-          <span className="title-remote" style={{ fontSize: '1.6em', fontWeight: 'bold' }}>Remote</span>
-          <div className="title-trends rounded-4 py-2 px-3 mx-1">
-            <span style={{ fontSize: '1.8em', fontWeight: 'bold' }}>Trends</span>
+      <div className="topbar fixed-top text-center p-3">
+        <div className="title-container rounded-3 p-1 mt-1 bg-light">
+          <img src="/favic.ico" alt="RemoteTrends Logo" className='mt-1' style={{ width: '40px', height: '40px', marginRight: '5px', marginBottom: '5px' }} />
+          <span className="title-remote" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>Remote</span>
+          <div className="title-trends rounded-4 py-2 px-3 m-1">
+            <span style={{ fontSize: '1.4em', fontWeight: 'bold' }}>Trends</span>
           </div>
         </div>
       </div>
