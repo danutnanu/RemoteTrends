23bd7f74f05bf1b162d452ff6288ff90830589a0
danut nanu <125583127+danutnanu@users.noreply.github.com>
2025-03-19 10:45:53 +0000

    Remove logo and title from SalaryPage as they are now handled by Topbar component.

--- src/components/pages/SalaryPage/SalaryPage.jsx
+++ src/components/pages/SalaryPage/SalaryPage.jsx
@@ -122,15 +122,6 @@ const SalaryPage = () => {
 
     return (
         <div className="main-content">
-            <div className="fixed-top fixed-header text-center">
-                <div className="title-container rounded-3 p-1 mt-1">
-                    <img src="/favic.ico" alt="RemoteTrends Logo" style={{ width: '40px', height: '40px', marginRight: '5px', marginBottom: '5px' }} />
-                    <span className="title-remote" style={{ fontSize: '1.6em', fontWeight: 'bold' }}>Remote</span>
-                    <div className="title-trends rounded-4 py-2 px-3 mx-1">
-                        <span style={{ fontSize: '1.8em', fontWeight: 'bold' }}>Trends</span>
-                    </div>
-                </div>
-            </div>
             <div className="charts-container border border-danger p-lg-5">
                 <div className="top-charts my-5 pt-3 border border-info">
                     <TopJobsChart />
