090a927b2c5d9c2d5d5aa5625b616eadbb09e441
danut nanu <125583127+danutnanu@users.noreply.github.com>
2025-03-19 07:30:07 +0000

    refactor(sidebar): enhance Bootstrap utility class usage
    - Added Bootstrap utility classes for spacing and layout
    - Improved responsive behavior with Bootstrap classes
    - Simplified component styling by reducing custom CSS dependencies

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,34 +12,34 @@ const Sidebar = () => {
   return (
     <>
       <BootstrapNavbar expand="lg" className="navbar-cus d-lg-none fixed-top">
-        <BootstrapNavbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShow} className="navbar-toggler mt-3" />
-        <div className="navigation-bar pb-3 mt-4 justify-content-end">
+        <BootstrapNavbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShow} className="navbar-toggler mt-4 ms-2" />
+        <div className="navigation-bar pb-3 mt-4 me-5">
           <img src="/favic.ico" alt="RemoteTrends Logo" style={{ width: '30px', height: '40px', marginRight: '10px', marginLeft: '30px', paddingTop: '10px' }} />
           <span style={{ color: '#fff', fontSize: '1.7em', fontWeight: 'bold', borderBottom: '2px solid #fff', paddingTop: '10px' }}>RemoteTrends</span>
         </div>
       </BootstrapNavbar>
 
-      <Offcanvas show={show} onHide={handleClose} placement="start" id="offcanvasNavbar" className="offcanvas-custom">
+      <Offcanvas show={show} onHide={handleClose} placement="start" id="offcanvasNavbar" className="offcanvas-custom" style={{ width: '250px', height: '100%', paddingTop: '30px' }}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Menu</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <Nav className="flex-column">
-            <Nav.Link as={Link} to="/" onClick={handleClose}>Location Page</Nav.Link>
-            <Nav.Link as={Link} to="/salary" onClick={handleClose}>Salary Page</Nav.Link>
-            <Nav.Link as={Link} to="/number" onClick={handleClose}>Number Page</Nav.Link>
+          <Nav className="flex-column ms-2 fs-6">
+            <Nav.Link as={Link} to="/" onClick={handleClose}>by Location</Nav.Link>
+            <Nav.Link as={Link} to="/salary" onClick={handleClose}>by Salary</Nav.Link>
+            <Nav.Link as={Link} to="/number" onClick={handleClose}>by Industry</Nav.Link>
           </Nav>
         </Offcanvas.Body>
       </Offcanvas>
 
-      <div className="sidebar d-none d-lg-block pt-5 mt-5">
-        <div className="sidebar-header mb-5 p-2 fs-5 border-bottom">
+      <div className="sidebar d-none d-lg-block pt-5 px-5 mt-3" style={{ width: '250px', height: '100%'}}>
+        <div className="sidebar-header mb-5 p-2 fs-4 border-bottom">
           Menu
         </div>
-        <Nav className="flex-column ms-2 fs-5">
-          <Nav.Link as={Link} to="/">Location Page</Nav.Link>
-          <Nav.Link as={Link} to="/salary">Salary Page</Nav.Link>
-          <Nav.Link as={Link} to="/number">Number Page</Nav.Link>
+        <Nav className="flex-column ms-2 fs-6">
+          <Nav.Link as={Link} to="/">by Location</Nav.Link>
+          <Nav.Link as={Link} to="/salary">by Salary</Nav.Link>
+          <Nav.Link as={Link} to="/number">by Industry</Nav.Link>
         </Nav>
       </div>
     </>
