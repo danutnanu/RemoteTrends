4e5d560f2e3d8ee2db02a7f088eb2acff6d39ca9
danut nanu <125583127+danutnanu@users.noreply.github.com>
2025-03-20 09:54:10 +0000

    Remove Bootstrap class

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -4,7 +4,7 @@ import './Topbar.css'
 const Topbar = () => {
   return (
     <>
-      <div className="topbar fixed-top text-center p-3 me-2 border border-1 rounded">
+      <div className="topbar fixed-top text-center p-3 me-2 border border-1">
         <div className="title-container rounded-3 p-1 mt-1 bg-light">
           <img src="/favic.ico" alt="RemoteTrends Logo" className='mt-1' style={{ width: '40px', height: '40px', marginRight: '5px', marginBottom: '5px' }} />
           <span className="title-remote" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>Remote</span>
