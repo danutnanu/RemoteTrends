040dfee45b7fdc3cd62901436d9df38e81da7d6b
danut nanu <125583127+danutnanu@users.noreply.github.com>
2025-03-23 08:06:29 +0000

    Refactor Topbar.jsx for improved styling and layout
    - Removed inline CSS and applied Bootstrap classes
    - Adjusted padding and margin for the main container to streamline layout
    - Enhanced responsiveness and maintainability by leveraging Bootstrap utilities

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -5,9 +5,9 @@ const Topbar = () => {
   return (
     <>
       <div className="topbar fixed-top text-center p-3 me-2 border border-1">
-        <div className="title-container rounded-3 p-1 mt-1 bg-light">
-          <img src="/inc.ico" alt="RemoteTrends Logo" className='mt-2' style={{ width: '30px', height: '30px', marginRight: '5px' }} />
-          <span className="title-remote" style={{ fontSize: '1.5em', fontWeight: 'bold' }}>Remote</span>
+        <div className="title-container rounded-3 bg-light">
+          <img src="/inc.ico" alt="RemoteTrends Logo" className='mt-3 ms-2' style={{ width: '30px', height: '30px' }} />
+          <span className="title-remote mt-1 fs-4 fw-bold">Remote</span>
           <div className="title-trends rounded-4 py-2 px-3 m-1">
             <span style={{ fontSize: '1.4em', fontWeight: 'bold' }}>Trends</span>
           </div>
