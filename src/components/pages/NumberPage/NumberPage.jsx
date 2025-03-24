92e9006a2b618924522e2cefd07def9cd4f4ea12
danut nanu <125583127+danutnanu@users.noreply.github.com>
2025-03-24 12:46:40 +0000

    Add Bootstrap classes to NumberPage.jsx

--- src/components/pages/NumberPage/NumberPage.jsx
+++ src/components/pages/NumberPage/NumberPage.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 const NumberPage = () => {
     return (
         <div className="main-content">
-            <div className="header pe-5 p-3 fixed-top">
-                <img src="/public/favic.ico" alt="RemoteTrends Logo" style={{ width: '30px', height: '30px', marginRight: '10px', marginLeft: '30px' }} />
-                <span style={{ color: '#fff', fontSize: '1.6em', fontWeight: 'bold' }}>RemoteTrends</span>
-            </div>
-            <div className="charts-container px-3">
-                <div id="chart-container" className="pt-3 rounded" style={{ width: '100%', height: '400px' }}></div>
+            <div className="container border border-danger p-2">
+                <div className="row m-4">
+                    <div className="col me-2 border border-info rounded" style={{ height: "75px" }}></div>
+                    <div className="col ms-2 border border-info">test</div>
+                </div>
+                <div className="row m-4">
+                    <div className="col border border-info">test</div>
+                </div>
             </div>
         </div>
     );
