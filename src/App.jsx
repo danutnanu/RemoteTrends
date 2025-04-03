3ceffe39c24e9bad2dc24fd12f2812e6066d092e
danut nanu <125583127+danutnanu@users.noreply.github.com>
2025-04-03 15:07:03 +0100

    Add automatic scroll-to-top functionality
    - Implemented ScrollToTop component for better user experience.
    - Added automatic scroll reset when navigating between pages.
    - Restructured App.jsx to properly handle route changes.
    - Ensures consistent page positioning when switching between views.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Sidebar from './components/Sidebar/Sidebar'
 import LocationPage from './components/pages/LocationPage/LocationPage'
 import SalaryPage from './components/pages/SalaryPage/SalaryPage'
@@ -8,21 +8,42 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Topbar from './components/Topbar/Topbar'
 
-function App() {
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  
+  React.useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'instant'
+    });
+  }, [pathname]);
+
+  return null;
+}
+
+const AppContent = () => {
   return (
     <>
-      <Router>
-        <Sidebar />
-        <Topbar />
-        <Routes>
-          <Route path="/" element={<LocationPage />} />
-          <Route path="/salary" element={<SalaryPage />} />
-          <Route path="/number" element={<NumberPage />} />
-          <Route path="/location" element={<LocationPage />} />
-        </Routes>
-      </Router>
+      <ScrollToTop />
+      <Sidebar />
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<LocationPage />} />
+        <Route path="/salary" element={<SalaryPage />} />
+        <Route path="/number" element={<NumberPage />} />
+        <Route path="/location" element={<LocationPage />} />
+      </Routes>
     </>
-  )
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
 }
 
 export default App
